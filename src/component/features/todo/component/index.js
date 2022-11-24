1fda06269e9b3d7fb001e99dae2b44d351841ace
thanhtu <daothanhtu1996@gmail.com>
2022-11-24 16:10:37 +0700

    bai1vs bai2

--- src/component/features/todo/component/index.js
+++ src/component/features/todo/component/index.js
@@ -1,62 +1,53 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
-import TodoList from "./TodoList";
+import data from "../../../data.json"
 
 TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-  const initTodoList = [
-    {
-      id: 1,
-      title: "eat",
-      status: "completed",
-    },
-    {
-      id: 2,
-      title: "sleep",
-      status: "new",
-    },
-    {
-      id: 3,
-      title: "code",
-      status: "new",
-    },
-  ];
+  const initTodoList =data;
+  const listItems = initTodoList
+    .slice(0, 24)
+    .map((initTodoList) => <li>{initTodoList}</li>);
+  console.log("1", listItems);
 
-  const [todoList, setTodoList] = useState(initTodoList);
-  const [filterList, setFilter] = useState('all');
+  const items = listItems.slice(0, 18).map((listItems) => <li>{listItems}</li>);
+  console.log("2", items);
 
-  const handleTodoClick = (todo, idx) => {
-    //clone cureent array to the new one
-    const newTodoList = [...todoList];
-    //toggle state
-    newTodoList[idx] = {
-      ...newTodoList[idx],
-      status: newTodoList[idx].status === "new" ? "completed" : "new",
-    };
-    //update totolist
-    setTodoList(newTodoList);
-  };
+  const detailItems = items.slice(1, 7).map((items) => <li>{items}</li>);
+  console.log("3", detailItems);
 
-  const handleALL = () => {
-    setFilter ('all')
-  };
-  const handleCompleted = () => {
-    setFilter ('completed')
-  };
-  const handleNew = () => {
-    setFilter ('new')
-  };
-  const renderList =todoList.filter(todo =>filterList ==='all'|| filterList===todo.status)
+  const a = [0, 10, 1, 99, 9, 8, 79, 91, 22, 32, 12];
+
+  let singleNumberArray = [];
+  let total;
+
+  a.forEach(
+    (v) =>
+      (singleNumberArray = singleNumberArray.concat(v.toString().split("")))
+  );
+  singleNumberArray = singleNumberArray.sort((a, b) => {
+    return b - a;
+  });
+  total = singleNumberArray.join("");
+
+  console.log(total);
 
   return (
     <div>
-      <h3>todoList</h3>
-      <TodoList todoList={renderList} onToClick={handleTodoClick} />
+      <h3>
+        2.Tạo 1 danh sách gồm 24 phần tử không trùng lặp nhau từ file JSON cho
+        trước. . .
+      </h3>
+      <h3>Từ danh sách 24 phần tử </h3>
+      <ul>{listItems}</ul>
 
-      <button onClick={handleALL}>handleALL</button>
-      <button onClick={handleNew}>handleNew</button>
-      <button onClick={handleCompleted}>handleCompleted</button>
+      <h3>tạo ra 1 danh sách gồm 18 phần tử không trùng lặp</h3>
+      <ul>{items}</ul>
+
+      <h3>Từ 18 phần tử vừa lấy được tạo thành 1 danh sách gồm 6 phần tử</h3>
+      <ul>{detailItems}</ul>
+
+      
     </div>
   );
 }
